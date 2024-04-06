b712d921566a1b762bcc70697a9f172b98bb6811
Kenric <kenric.chui@gmail.com>
2024-04-06 22:34:59 +1100

    push

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // http://www.omdbapi.com/?apikey=1ee8e7af&/
+
+const searchInput = document.querySelector("[data-search]");
+searchInput.addEventListener("input", (e) => {
+  const value = e.target.value.toLowerCase();
+  console.log(value);
+});
+
+async function main() {
+  const data = await fetch(`http://www.omdbapi.com/?apikey=1ee8e7af&s=${value}`);
+  const movieData = await data.json();
+  console.log(movieData);
+}
+main();
