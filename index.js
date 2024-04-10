2a4ffab3c0c3c375c82678e8398872dff080207c
Kenric <kenric.chui@gmail.com>
2024-04-10 19:03:31 +1000

    push

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 // http://www.omdbapi.com/?apikey=1ee8e7af&/
 
-const searchInput = document.querySelector("[data-search]");
-searchInput.addEventListener("input", (e) => {
-  const value = e.target.value.toLowerCase();
-  console.log(value);
-});
+function logSubmit(event) {
+  event.preventDefault();
+  console.log(event);
+  console.log(log)
+}
+
+const form = document.getElementById("form");
+const log = document.getElementById("search");
+form.addEventListener("submit", logSubmit);
 
 async function main() {
-  const data = await fetch(`http://www.omdbapi.com/?apikey=1ee8e7af&s=${value}`);
+  const data = await fetch(`http://www.omdbapi.com/?apikey=1ee8e7af&s=big`);
   const movieData = await data.json();
   console.log(movieData);
+  const movieWrapperEl = document.querySelector(".movie__wrapper");
+  movieWrapperEl.innerHTML = movieData.Search
+    .map((Search) => movieHTML(Search))
+    .join("");
 }
+
 main();
+
+function movieHTML(Search) {
+  return ` <div class="movie">
+  <img class="movie__img" src="${Search.Poster}" alt="">
+  <h1 class="movie__title">${Search.Title}</h1>
+  <h2 class="movie__year">Year of Release: ${Search.Year}</h2>
+  <h6 class="movie__id">Movie imdbID: ${Search.imdbID}</h6>
+</div>`;
+}
+movieHTML();
